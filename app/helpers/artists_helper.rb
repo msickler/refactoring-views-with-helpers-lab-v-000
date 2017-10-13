0d9245b47292292f7bc8eaea3481c16f8fa397f4
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-13 13:48:02 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,10 +1,10 @@
 module ArtistsHelper
 
   def display_artist(song)
-    if song.artist != nil 
+    if song.artist != nil
       link_to song.artist.name, song.artist
-    else 
+    else
       link_to 'Add Artist' edit_song_path(song)
-    end 
+    end
   end
 end
